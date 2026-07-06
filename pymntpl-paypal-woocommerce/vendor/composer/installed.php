c9abe248315a38339c735882069c14e29d5c0770
Sean Murphy <sean@iamseanmurphy.com>
2026-07-06 09:09:11 +0000

    2.0.21

--- pymntpl-paypal-woocommerce/vendor/composer/installed.php
+++ pymntpl-paypal-woocommerce/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '2.0.20.x-dev',
-        'version' => '2.0.20.9999999-dev',
-        'reference' => '20e1b6e958a03511d38516d93e1310e4457b4304',
+        'pretty_version' => '2.0.21.x-dev',
+        'version' => '2.0.21.9999999-dev',
+        'reference' => '35c2eeafc2aa397cb316d87783e19177ecc94043',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '2.0.20.x-dev',
-            'version' => '2.0.20.9999999-dev',
-            'reference' => '20e1b6e958a03511d38516d93e1310e4457b4304',
+            'pretty_version' => '2.0.21.x-dev',
+            'version' => '2.0.21.9999999-dev',
+            'reference' => '35c2eeafc2aa397cb316d87783e19177ecc94043',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -22,7 +22,7 @@
         'paymentplugins/paypal-php-sdk' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '021e4831012a34b6f16469b7f9cc84c65e3b651e',
+            'reference' => '633eead6e7f487f16d67a0ab0f1c2cad993ab005',
             'type' => 'library',
             'install_path' => __DIR__ . '/../paymentplugins/paypal-php-sdk',
             'aliases' => array(
